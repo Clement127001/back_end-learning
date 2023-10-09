afe18b2ec619e355c551be586afad55d0d5be554
Clement127001 <babiyonclement18@gmail.com>
2023-10-09 19:02:33 +0530

    add the first practise file

---
+++ node_fundamentals/app.js
@@ -0,0 +1,6 @@
+const weight = 50;
+
+if (weight > 50) console.log("You are heavier than I think");
+else console.log("You are lighter that I think");
+
+console.log("This is the second phase of the entire journey");
