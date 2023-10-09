f3f79131254db1cd9258f863180efc7c331ed0ec
Clement127001 <babiyonclement18@gmail.com>
2023-10-09 19:19:08 +0530

    change file name

--- node_fundamentals/1_intro.js
+++ node_fundamentals/1_intro.js
@@ -1,5 +1,6 @@
 const weight = 50;
 
+//node is the runtime that helps to create server side code
 if (weight > 50) console.log("You are heavier than I think");
 else console.log("You are lighter that I think");
 
