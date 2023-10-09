4f264df6e340afd1a73f0633fd17d7b16e68d27d
Clement127001 <babiyonclement18@gmail.com>
2023-10-09 19:35:18 +0530

    add the global variable example

---
+++ node_fundamentals/app.js
@@ -0,0 +1,14 @@
+//this file is all about the global variables
+//some of the list of all global varibales used inside the node env
+
+//process -> very important
+//modules
+//require
+//__filename
+//__dirname
+
+console.log("Dir name : " + __dirname);
+console.log("Require info : " + require);
+console.log("Modules : " + module);
+console.log("File name: " + __filename);
+console.log("Process : " + process);
