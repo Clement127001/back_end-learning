6b14004a300597f72fc0d0f6576369d24cf70dc2
Clement127001 <babiyonclement18@gmail.com>
2023-10-10 19:35:00 +0530

    add extra function

--- node_fundamentals/3_working_with_modules.js
+++ node_fundamentals/3_working_with_modules.js
@@ -3,14 +3,10 @@
 
 //here the functionality is cut out to the other file(module,because every file is module by default)
 const { jerry: j, isabella: i } = require("./4_name");
-const sayThankYou = require("./5_utils");
+const { sayThankYou, myDetail } = require("./5_utils");
 require("./6_unknow_thing");
 
-console.log(module);
+sayThankYou(j);
+sayThankYou(i);
 
-//just a check
-const myName = () => {
-  console.log("I am clement and I wanna be a best full stack developer");
-};
-
-module.exports = myName;
+myDetail();
--- node_fundamentals/5_utils.js
+++ node_fundamentals/5_utils.js
@@ -4,9 +4,14 @@ const sayThankYou = (name) => {
   console.log(`Thank you ${name}`);
 };
 
+//just a check
+const myDetail = () => {
+  console.log("I am clement and I wanna be a best full stack developer");
+};
+
 //the two way of exporting
 
 //1 -> using object like
 //2 -> using default export used commonly in exporting the componets in react js
 
-module.exports = sayThankYou;
+module.exports = { sayThankYou, myDetail };
